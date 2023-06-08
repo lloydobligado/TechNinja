47a60eb85bed729df0713dee0899c3325aab44b0
Lloyd Carie Mendoza Obligado <lobligado@intercommerce.com.ph>
2023-06-08 21:13:24 +0800

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NotFound from './pages/NotFound'
 
 const App = () => {
   return (
-    <div className='text-red-500'>App</div>
+    <>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='*' element={<NotFound/>}/>
+      </Routes>
+    </>
   )
 }
 
